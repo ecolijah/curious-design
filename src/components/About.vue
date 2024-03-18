84838a101a3079bc0da5c038be64ccf1c26b41dc
ecolijah <ecolijah@gmail.com>
2024-03-18 18:09:05 -0500

    working on about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,24 @@
 <template>
-    <div>
-        <h1>about section</h1>
+    <div class="container">
+        <div class="details">
+            <h1>about </h1>
+
+        </div>
+        <div class="image-container">
+            <img src="../assets/about-me.png" alt="aboutmepic" />
+        </div>
     </div>
     
 </template>
 
 <style scoped>
+
+img {
+        /* width: 1920px; */
+        /* height: 90vh; */
+        max-width: 100%; /* Maximum width of the image */
+        max-height: 100%; /* Maximum height of the image */
+        object-fit: contain; /*Maintain aspect ratio and fit within the container*/
+
+    }
 </style>
